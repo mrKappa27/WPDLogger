1bd48088d6a136e35dc16497dfb5c1a44d64c396
mrKappa27 <kevvort@hotmail.it>
2023-05-22 21:51:28 +0200

    first commit

---
+++ WPDLogger.ino
@@ -0,0 +1,139 @@
+//https://randomnerdtutorials.com/esp32-data-logging-temperature-to-microsd-card/
+
+// Libraries for SD card
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
+
+
+// Define deep sleep options
+uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+// Sleep for 10 minutes = 600 seconds
+uint64_t TIME_TO_SLEEP = 600;
+
+// Define CS pin for the SD card module
+#define SD_CS 5
+
+// Save reading number on RTC memory
+RTC_DATA_ATTR int readingID = 0;
+
+String dataMessage;
+
+// Pressure Sensor variables
+float pressure;
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
+
+void setup() {
+  // Start serial communication for debugging purposes
+  Serial.begin(115200);
+
+  Serial.print("Booting...");
+
+  // Initialize SD card
+  SD.begin(SD_CS);  
+  if(!SD.begin(SD_CS)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("ERROR - SD card initialization failed!");
+    return;    // init failed
+  }
+
+  // If the data.txt file doesn't exist
+  // Create a file on the SD card and write the data labels
+  File file = SD.open("/data.txt");
+  if(!file) {
+    Serial.println("File doens't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/data.txt", "Reading ID, Date, Hour, Pressure \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
+
+  // Enable Timer wake_up
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  getReadings();
+  getTimeStamp();
+  logSDCard();
+  
+  // Increment readingID on every new reading
+  readingID++;
+  
+  // Start deep sleep
+  Serial.println("DONE! Going to sleep now.");
+  esp_deep_sleep_start(); 
+}
+
+void loop() {
+  // The ESP32 will be in deep sleep
+  // it never reaches the loop()
+}
+
+// Function to get pressure
+void getReadings(){
+  
+  Serial.print("Pressure: ");
+  Serial.println(pressure);
+}
+
+// Function to get date and time from NTPClient
+void getTimeStamp() {
+  //Placeholder
+}
+
+// Write the sensor readings on the SD card
+void logSDCard() {
+  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," + 
+                String(pressure) + "\r\n";
+  Serial.print("Save data: ");
+  Serial.println(dataMessage);
+  appendFile(SD, "/data.txt", dataMessage.c_str());
+}
+
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
